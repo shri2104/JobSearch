2bfb92555dcf5978eb9590b5246de0ca17b0e034
shri2104 <shrikants0421@gmail.com>
2025-04-11 00:39:24 +0530

    git config --local --list

--- app/src/main/java/com/example/jobsearch/Screens/Jobpostingscreen.kt
+++ app/src/main/java/com/example/jobsearch/Screens/Jobpostingscreen.kt
@@ -2,6 +2,8 @@ package com.example.jobsearch.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -15,6 +17,7 @@ import androidx.navigation.NavHostController
 import com.example.jobsearch.viewmodel.JobViewModel
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun JobPostScreen(
     viewModel: JobViewModel = hiltViewModel(),
@@ -26,9 +29,10 @@ fun JobPostScreen(
     var organizationName by remember { mutableStateOf("") }
     var category by remember { mutableStateOf("") }
     var skillsRequired by remember { mutableStateOf("") }
-    var status by remember { mutableStateOf("Applied") }
+    val status = "Applied"
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+
     LaunchedEffect(jobId) {
         jobId?.let {
             val jobDetails = viewModel.getJobById(it.toInt())
@@ -37,78 +41,80 @@ fun JobPostScreen(
                 organizationName = jobDetails.organizationName
                 category = jobDetails.category
                 skillsRequired = jobDetails.skillsRequired.joinToString(", ")
-                status = jobDetails.status
             }
         }
     }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = if (jobId != null) "Update Job" else "Post a Job") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .align(Alignment.Center)
-        ) {
-            InputFields(label = "Job Title", value = title, onValueChange = { title = it }, placeholder = "Enter job title")
-            InputFields(label = "Organization Name", value = organizationName, onValueChange = { organizationName = it }, placeholder = "Enter organization name")
-            InputFields(label = "Job Category", value = category, onValueChange = { category = it }, placeholder = "Enter job category")
-            InputFields(label = "Skills Required", value = skillsRequired, onValueChange = { skillsRequired = it }, placeholder = "Enter required skills")
-            val statuses = listOf("Applied", "Interview", "Offer", "Rejected")
-            Column(modifier = Modifier.padding(vertical = 16.dp)) {
-                Text("Status", style = MaterialTheme.typography.headlineSmall)
-                statuses.forEach { statusOption ->
-                    Row(modifier =Modifier.padding(8.dp)) {
-                        RadioButton(
-                            selected =(status == statusOption),
-                            onClick ={ status = statusOption }
-                        )
-                        Spacer(modifier =Modifier.width(8.dp))
-                        Text(text =statusOption)
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "Back")
                     }
                 }
-            }
+            )
+        },
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { paddingValues ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.Center)
+            ) {
+                InputFields(label = "Job Title", value = title, onValueChange = { title = it }, placeholder = "Enter job title")
+                InputFields(label = "Organization Name", value = organizationName, onValueChange = { organizationName = it }, placeholder = "Enter organization name")
+                InputFields(label = "Job Category", value = category, onValueChange = { category = it }, placeholder = "Enter job category")
+                InputFields(label = "Skills Required", value = skillsRequired, onValueChange = { skillsRequired = it }, placeholder = "Enter required skills")
 
-            Row {
-                Button(onClick = {
-                    if (title.isBlank() || organizationName.isBlank() ||
-                        category.isBlank() || skillsRequired.isBlank()) {
-
-                        coroutineScope.launch {
-                            snackbarHostState.showSnackbar("Please fill in all fields")
-                        }
-                    } else {
-                        val jobAction = Job(
-                            id = jobId?.toInt() ?: 0,
-                            title = title,
-                            organizationName = organizationName,
-                            category = category,
-                            skillsRequired = skillsRequired.split(",").map { it.trim() },
-                            status = status
-                        )
-
-                        if (jobId != null) {
-                            viewModel.updateJob(jobAction)
+                Row {
+                    Button(onClick = {
+                        if (title.isBlank() || organizationName.isBlank() || category.isBlank() || skillsRequired.isBlank()) {
+                            coroutineScope.launch {
+                                snackbarHostState.showSnackbar("Please fill in all fields")
+                            }
                         } else {
-                            viewModel.postJob(jobAction)
+                            val jobAction = Job(
+                                id = jobId?.toInt() ?: 0,
+                                title = title,
+                                organizationName = organizationName,
+                                category = category,
+                                skillsRequired = skillsRequired.split(",").map { it.trim() },
+                                status = status
+                            )
+
+                            if (jobId != null) {
+                                viewModel.updateJob(jobAction)
+                            } else {
+                                viewModel.postJob(jobAction)
+                            }
+
+                            title = ""
+                            organizationName = ""
+                            category = ""
+                            skillsRequired = ""
+                            onJobPosted()
                         }
-                        title = ""
-                        organizationName = ""
-                        category = ""
-                        skillsRequired = ""
-                        status = "Applied"
-                        onJobPosted()
+                    }) {
+                        Text(if (jobId != null) "Update Job" else "Post Job")
                     }
-                }) {
-                    Text(if (jobId != null) "Update Job" else "Post Job")
-                }
-                Spacer(modifier = Modifier.width(50.dp))
-                Button(onClick = {navController.navigate("joblisting") }) {
-                    Text(text = "Jobs")
+
+                    Spacer(modifier = Modifier.width(50.dp))
+
+                    Button(onClick = { navController.navigate("joblisting") }) {
+                        Text(text = "Jobs")
+                    }
                 }
             }
         }
-        SnackbarHost(hostState = snackbarHostState, modifier = Modifier.align(Alignment.TopCenter))
     }
 }
+
+
 @Composable
 fun InputFields(label: String, value: String, onValueChange: (String) -> Unit, placeholder: String) {
     Text(
